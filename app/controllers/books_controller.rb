f66c1161912791d23eacf77155f9f247d441efb8
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-09-03 13:29:42 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,19 @@
 class BooksController < ApplicationController
   def new
+  @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user= current_user
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def index
     @user = current_user
     @books = Book.all
+    @book = Book .new
   end
 
   def show
@@ -12,4 +21,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
